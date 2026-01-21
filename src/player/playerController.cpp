cb9d915266d855b10dc4ff48179bc0758f47d9d0
Joshua Wolff <wolff.joshua2002@icloud.com>
2026-01-21 13:41:57 +0100

    feat: player camera rotation

--- src/player/playerController.cpp
+++ src/player/playerController.cpp
@@ -23,6 +23,14 @@ void PlayerController::_bind_methods() {
     ClassDB::bind_method(D_METHOD("getMouseDebugState"), &PlayerController::getMouseDebugState);
 
     ADD_PROPERTY(PropertyInfo(Variant::BOOL, "showMouseMotion"), "setMouseDebugState", "getMouseDebugState");
+
+    ClassDB::bind_method(D_METHOD("setVerticalMouseSpeed", "m_verticalMouseSpeed"), &PlayerController::setVerticalMouseSpeed);
+    ClassDB::bind_method(D_METHOD("getVerticalMouseSpeed"), &PlayerController::getVerticalMouseSpeed);
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "Vertical Mouse Speed"), "setVerticalMouseSpeed", "getVerticalMouseSpeed");
+
+    ClassDB::bind_method(D_METHOD("setHorizontalMouseSpeed", "m_horizontalMouseSpeed"), &PlayerController::setHorizontalMouseSpeed);
+    ClassDB::bind_method(D_METHOD("getHorizontalMouseSpeed"), &PlayerController::getHorizontalMouseSpeed);
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "Horizontal Mouse Speed"), "setHorizontalMouseSpeed", "getHorizontalMouseSpeed");
 }
 
 //
@@ -30,6 +38,7 @@ void PlayerController::_bind_methods() {
 //
 
 void PlayerController::_ready() {
+    set_position({0, 1, 0});
     try {
         m_camera = get_node<Camera3D>("Camera3D");
         m_camera->set_current(true);
@@ -37,23 +46,78 @@ void PlayerController::_ready() {
     } catch (int errorCode) {
         print_error("Failed to set player camera as main");
     }
+
+    Input::get_singleton()->set_mouse_mode(Input::MOUSE_MODE_CAPTURED);
+
+    set_process(true);
+}
+
+void PlayerController::_process(double delta) {
+    handleCamera(delta);
 }
 
 void PlayerController::_input(const Ref<InputEvent> &a_event) {
     Ref<InputEventMouseMotion> mouseMotion = a_event;
     if (mouseMotion.is_valid()) {
-        Vector2 rel = mouseMotion->get_relative();
-        if (m_showMouseMotion)
-            UtilityFunctions::print("X: ", rel.x, " Y: ", rel.y);
+        m_mouseInput = mouseMotion->get_relative();
+
+        if (m_showMouseMotion) // debug mouse motion
+            UtilityFunctions::print("X: ", m_mouseInput.x, " Y: ", m_mouseInput.y);
+    }
+
+    Ref<InputEventKey> keyEvent = a_event;
+    if (keyEvent.is_valid() && keyEvent->is_pressed()) {
+        if (keyEvent->get_keycode() == Key::KEY_ESCAPE) {
+            Input::MouseMode mode = Input::get_singleton()->get_mouse_mode();
+            if (mode == Input::MOUSE_MODE_CAPTURED) {
+                Input::get_singleton()->set_mouse_mode(Input::MOUSE_MODE_VISIBLE);
+            }
+            else {
+                Input::get_singleton()->set_mouse_mode(Input::MOUSE_MODE_CAPTURED);
+            }
+            m_enableMovement = !m_enableMovement;
+        }
     }
 }
 
 //
-// Public Methods
+// Getter/Setter Methods
 //
 void PlayerController::setMouseDebugState(const bool a_debugState) {
     m_showMouseMotion = a_debugState;
 }
 bool PlayerController::getMouseDebugState() const {
     return m_showMouseMotion;
+}
+
+void PlayerController::setVerticalMouseSpeed(const float a_mouseSpeed) {
+    m_verticalMouseSpeed = a_mouseSpeed;
+}
+float PlayerController::getVerticalMouseSpeed() const {
+    return m_verticalMouseSpeed;
+}
+
+void PlayerController::setHorizontalMouseSpeed(const float a_mouseSpeed) {
+    m_horizontalMouseSpeed = a_mouseSpeed;
+}
+float PlayerController::getHorizontalMouseSpeed() const {
+    return m_horizontalMouseSpeed;
+}
+
+// 
+// Movement control
+//
+void PlayerController::handleCamera(const double delta) {
+    if (!m_enableMovement)
+        return;
+
+    // Set horizontal rotation
+    Vector3 rotation_main = get_rotation();
+    rotation_main.y += Math::deg_to_rad(m_mouseInput.x * m_horizontalMouseSpeed * delta);
+    set_rotation(rotation_main);
+
+    // Set vertical rotation
+    Vector3 rotation_camera = m_camera->get_rotation();
+    rotation_camera.x += Math::deg_to_rad(m_mouseInput.y * m_verticalMouseSpeed * delta);
+    m_camera->set_rotation(rotation_camera);
 }
--- src/player/playerController.h
+++ src/player/playerController.h
@@ -4,9 +4,14 @@
 #include <godot_cpp/classes/node3d.hpp>
 #include <godot_cpp/classes/camera3d.hpp>
 #include <godot_cpp/core/class_db.hpp>
+
+#include <godot_cpp/classes/input.hpp>
 #include <godot_cpp/classes/input_event.hpp>
 #include <godot_cpp/classes/input_event_mouse_motion.hpp>
+#include <godot_cpp/classes/input_event_key.hpp>
+
 #include <godot_cpp/variant/utility_functions.hpp>
+#include <godot_cpp/variant/transform3d.hpp>
 
 
 namespace godot {
@@ -20,6 +25,16 @@ private:
     // debug
     bool m_showMouseMotion = false;
 
+    
+    float m_verticalMouseSpeed = 10.0f;
+    float m_horizontalMouseSpeed = 10.0f;
+
+
+    bool m_enableMovement = true;
+    Vector2 m_mouseInput = {0, 0};
+
+    void handleCamera(const double delta);
+
 protected:
     static void _bind_methods();
 
@@ -28,10 +43,16 @@ public:
     ~PlayerController();
 
     void _ready() override;
+    void _process(double delta) override;
     void _input(const Ref<InputEvent> &a_event);
 
     void setMouseDebugState(const bool a_debugState);
     bool getMouseDebugState() const;
+
+    void setVerticalMouseSpeed(const float a_mouseSpeed);
+    float getVerticalMouseSpeed() const;
+    void setHorizontalMouseSpeed(const float a_mouseSpeed);
+    float getHorizontalMouseSpeed() const;
 };
 
 }
